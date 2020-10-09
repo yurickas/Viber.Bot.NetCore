278f45802942fe2525c6d921aa18a3d14b77cda5
Юрик Хмелюк <yurick.hmeluk@gmail.com>
2020-10-09 09:33:37 +0300

    add webhooks

--- Viber.Bot.NetCore/Middleware/ViberMiddlewareExtentions.cs
+++ Viber.Bot.NetCore/Middleware/ViberMiddlewareExtentions.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Viber.Bot.NetCore.Infrastructure;
+using Viber.Bot.NetCore.Models;
 
 namespace Viber.Bot.NetCore.Middleware
 {
@@ -7,6 +11,28 @@ namespace Viber.Bot.NetCore.Middleware
     {
         public static IServiceCollection AddViberBotApi(this IServiceCollection services, IConfiguration configuration)
         {
+            var section = configuration.GetSection("ViberBot");
+
+            services.AddViberBotApi(a =>
+            {
+                a.Token = section["Token"];
+                a.Webhook = section["Webhook"];
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddViberBotApi(this  IServiceCollection services, Action<ViberBotConfiguration> action)
+        {
+            ViberBotConfiguration conf = new ViberBotConfiguration();
+
+            action.Invoke(conf);
+
+            var bot = ViberClient.RegisterViberApi(conf);
+
+            bot.SetWebHookAsync(new ViberWebHook.WebHookRequest(conf.Webhook));
+
+            services.AddSingleton(bot);
 
             return services;
         }
--- Viber.Bot.NetCore/RestApi/IViberBotApi.cs
+++ Viber.Bot.NetCore/RestApi/IViberBotApi.cs
@@ -1,7 +1,12 @@
-﻿namespace Viber.Bot.NetCore.RestApi
+﻿using System.Threading.Tasks;
+using Refit;
+using Viber.Bot.NetCore.Models;
+
+namespace Viber.Bot.NetCore.RestApi
 {
     public interface IViberBotApi
     {
-        
+        [Post("/set_webhook")]
+        Task<ApiResponse<ViberWebHook.ViberWebHookResponse>> SetWebHookAsync([Body] ViberWebHook.WebHookRequest request);
     }
 }
